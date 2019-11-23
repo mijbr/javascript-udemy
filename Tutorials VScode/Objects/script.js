2c6bc2e964c977b6fe6408efc6ce1337f6af331c
mijbr <mijbr@mail.ru>
2019-11-23 19:51:50 +0000

    objects jane

--- Tutorials VScode/Objects/script.js
+++ Tutorials VScode/Objects/script.js
@@ -1,10 +1,39 @@
 var john = {
+
     firstname: 'john',
+
     lastname: 'smith',
+
     bithyear: 1990,
+
     family: ['jane', 'jack', 'bob', 'emily'],
+
     isMarried: false
+
 };
+
+
 
 console.log(john.firstname);
 
+console.log(john['lastname']);
+
+var x = 'birthyear';
+
+console.log(john[x]);
+
+
+
+var jane = new Object();
+
+
+
+jane.lastname = 'robinson';
+
+jane.firstname = 'jane';
+
+jane.dob = 1999;
+
+
+
+console.log(jane);
